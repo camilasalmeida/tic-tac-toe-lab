a7fef2fd84ad4aa4793f7244c0c2d02aa1480140
Camila Alm <camila.salmeidatech@hotmail.com>
2024-07-27 16:54:23 -0400

    Steps 1, 2 and 3 are now correct../

--- js/app.js
+++ js/app.js
@@ -16,28 +16,62 @@ const messageEl = document.querySelectorAll('#message');
 //console.dir(messageEl);
 
 
-/*-------------------------------- Functions --------------------------------*/
+/*-------------------------------- Functions--------------------------------*/
 
 //=>-----------------------------FUNCTION PART 1-----------------------------<=
-const init = () => {
+function init() {                          //CALL A FUNCTION CALLED `INIT`.
+    console.log('Init function is called!.');
+}
+window.onload = init;                      //THE `WINDOW.ONLOAD` EVENT IS USED TO CALL THE `INIT` FUNCTION WHEN THE WINDOW (OR APP) LOADS. THIS ENSURES THAT THE `INIT` FUNCTION IS EXECUTED AS SOON AS THE PAGE IS FULLY LOADED.
+
     board = ['', '', '', '', '', '', '', ''];
     turn = 'X';
     winner = false;
     tie = false;
 
-    render()
 
-    //console.log('Init function is called!');
-};
+function render() {                        //CALL A FUNCTION CALLED RENDER.
+    console.log('The board is rendered!.');
+
+}
+render();
+
 //----------------------------END OF FUNCTION PART 1--------------------------<= 
 
 
 
 
 //=>----------------------------FUNCTION PART 2-------------------------------<=
+//const render = () => {
+
+// };
+//--------------------------END OF FUNCTION PART 2--------------------------<= 
 
 
 
+
+
+//------------------------------FUNCTION PART 3--------------------------<= 
+// squareEls.forEach((squareEl, index) => {
+//     squareEl.textContent = board[index];
+//     squareEl.classList.remove('x', 'o');
+//     if (board[index] === 'X') {
+//       squareEl.classList.add('x');
+//     } else if (board[index] === 'O') {
+//       squareEl.classList.add('o');
+//     }
+//   });
+//---------------------------END OF FUNCTION PART 3--------------------------<= 
+
+
+
+
+//-------------------------------FUNCTION PART 4-----------------------------<= 
+// const updateMessage = () => {
+
+// };
+
+//---------------------------END OF FUNCTION PART 4--------------------------<= 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
